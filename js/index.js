1a1eb2763158c170ff2b31f1e8dddba6207cf79d
Lucas Guilherme <lguilherme8@gmail.com>
2021-10-26 20:56:24 -0300

    Corrigi a mostra do resultado

--- js/index.js
+++ js/index.js
@@ -1,30 +1,28 @@
+const input_result = document.querySelector('.input_result')
+const input_1 = document.querySelector("#num1")
+const input_2 = document.querySelector("#num2")
 
-let num1 = Number(document.querySelector("#num1").value)
-let num2 = Number(document.querySelector("#num2").value)
+let value_1 = input_1.value
+let value_2 = input_2.value
 
-function funcaoSoma(){
-    let soma = num1 + num2
-     console.log(soma)
- }
- console.log(num1, num2)
+input_1.addEventListener('change', (e) => {
+    value_1 = Number(e.target.value)
+})
+
+input_2.addEventListener('change', (e) => {
+    value_2 = Number(e.target.value)
+})
+
 
 const botao_soma = document.querySelector(".botao_soma")
-    botao_soma.addEventListener('click', funcaoSoma())
-
-let num1 = Number(num1.value)
-let value_2 = Number(input_2.value)
-    
-    
-input_1.addEventListener('change', (e) => {
-        value_1 = Number(e.target.value)
-    })
-    input_2.addEventListener('change', (e) => {
-        value_2 = Number(e.target.value)
+    botao_soma.addEventListener('click', () => {
+        let soma = value_1 + value_2
+        input_result.innerHTML = `${soma}`
     })
 
-     
+const botao_sub = document.querySelector('.botao_sub')
+    botao_sub.addEventListener('click', () => {
+        let sub = value_1 - value_2
+        input_result.innerHTML = `${sub}`
+    })
 
-
-/*
-const resultado = document.querySelector('.result_text')
-    console.log(resultado.innerHTML.toUpperCase()) */
